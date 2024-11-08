2268643c325b9c0f3a6cc26dcc9b052edc25e65b
nrmadi02 <ahmadinur2202@gmail.com>
2024-11-08 19:59:04 +0800

    refactor: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: [
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  plugins: ["import"],
+  rules: {
+    "tailwindcss/no-custom-classname": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+        ],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@app/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
+module.exports = config;
