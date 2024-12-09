dfe2f757b4ca099a5cdb711f08c68f50c0022819
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-09 09:08:19 +0800

    📦refactor: simplify ability logic by using session data instead of fetching role from db

--- src/lib/ability.ts
+++ src/lib/ability.ts
@@ -1,36 +1,21 @@
 import { AbilityBuilder, PureAbility } from "@casl/ability";
 import { Session } from "next-auth";
 
-import { db } from "~/server/db";
-
 type Actions = "manage" | "create" | "read" | "update" | "delete";
 type Subjects = string | "all";
 
 export type AppAbility = PureAbility<[Actions, Subjects]>;
 
-export async function defineAbilityFor(
+export function defineAbilityFor(
   user?: Session["user"]
   | null
-): Promise<AppAbility> {
+): AppAbility {
   const { can, rules } = new AbilityBuilder<PureAbility<[Actions, Subjects]>>(
     PureAbility,
   );
 
-  const roleWithPermissions = await db.role.findUnique({
-    where: {
-      id: user?.role.id,
-    },
-    include: {
-      permissions: {
-        include: {
-          subject: true,
-        },
-      },
-    },
-  })
-
-  if (roleWithPermissions) {
-    roleWithPermissions.permissions.forEach((permission) => {
+  if (user) {
+    user.permissions.forEach((permission) => {
       can(permission.action as Actions, permission.subject.name);
     })
   }
